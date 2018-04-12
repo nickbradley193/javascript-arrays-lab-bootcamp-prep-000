43fcd7b9d7f6fcc3725d57bf37786b9b1f4b8ba2
Nick Bradley <nick.bradley.193@pm.me>
2018-04-12 04:11:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,29 @@ function destructivelyAppendKitten(name) {
 function destructivelyPrependKitten(name) {
   kittens.unshift(name);
 }
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, kittens.length-1);
+  return newKittens;
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
